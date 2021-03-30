21bc7534dd9f05b009a6403e86aa01045f763b98
Chris Hall <51643851+Chrishall77@users.noreply.github.com>
2021-03-30 12:26:43 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        Authorization: 'Client-ID gWwjTIjfKGKv5NE2cUSxbDLroKLKaIrWRKaBWALebyM',
+        Authorization: 'Client-ID {put your access key here}',
     }
 });
